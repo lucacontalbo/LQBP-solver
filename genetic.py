2fd984c8393567efe000dec2eaaf4320726e6e5b
conta16 <luca.contalbo@gmail.com>
2021-03-31 19:32:47 +0200

    added creation of gen 0. Added code for initial values of slack variables

--- genetic.py
+++ genetic.py
@@ -1,5 +1,7 @@
 from LQBP import LQBP
+import numpy as np
 import lxml.etree
+import random
 
 class Genetic:
 	def __init__(self):
@@ -9,6 +11,7 @@ class Genetic:
 		self.max_generation = int(input("Insert max number of generations"))
 		self.gen_counter = 0
 		lqbp = LQBP()
+		self.population = np.array([]) #used to store the current population of chromosomes
 
 	def __init__(self,path):
 		with open(path) as f:
@@ -23,3 +26,15 @@ class Genetic:
 		self.gen_counter = 0
 
 		lqbp = LQBP(root)
+		self.population = []
+
+	def create_population(self): #function to be called to create generation 0 of chromosomes
+		i = 0
+		while i<self.population_size:
+			tmp = np.array([])
+			for j in range(self.lqbp.m+self.lqbp.ylength):
+				tmp = np.append(tmp,[random.randint(0,1)])
+			if tmp not in self.population:
+				self.population = np.append(self.population,tmp)
+				i += 1
+
